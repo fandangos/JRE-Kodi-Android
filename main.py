6c7255baa80113872730e1b7dcbb26f39cb5633a
fandangos <veggav@gmail.com>
2021-02-01 15:45:16 -0300

    Avoid kodi extracting the file in the addon folder

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os, sys, xbmc, time, stat, xbmcvfs, xbmcaddon
 from zipfile import ZipFile
 import shutil
 
-src = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'j2re-image.zip')
+src = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources/lib/j2re-image.zip')
 loc = xbmcvfs.translatePath("special://xbmcbin/j2re-image.zip")
 loc2 = xbmcvfs.translatePath("special://xbmcbin/j2re-image/")
 loc3 = xbmcvfs.translatePath("special://xbmcbin/")
@@ -12,8 +12,8 @@ if xbmcvfs.exists(loc2):
     shutil.rmtree(loc2)
     os.remove(loc)
 
-
-xbmcvfs.copy(src, loc)
+if xbmcvfs.exists(src):
+    xbmcvfs.copy(src, loc)
 
 zip = ZipFile(loc)
 zip.extractall(loc3)
@@ -23,3 +23,4 @@ zip.close()
 for path, subdirs, files in os.walk(loc2):
     for name in files:
         print(os.path.join(path, name))
+
