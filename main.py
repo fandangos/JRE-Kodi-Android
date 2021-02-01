c7ef394b1a132d1d5e61790ad87e85050511d138
fandangos <veggav@gmail.com>
2021-01-31 21:01:53 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.4
+import os, sys, xbmc, time, stat, xbmcvfs, xbmcaddon
+from zipfile import ZipFile
+import shutil
+
+src = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'j2re-image.zip')
+loc = xbmcvfs.translatePath("special://xbmcbin/j2re-image.zip")
+loc2 = xbmcvfs.translatePath("special://xbmcbin/j2re-image/")
+loc3 = xbmcvfs.translatePath("special://xbmcbin/")
+
+if xbmcvfs.exists(loc2):
+    shutil.rmtree(loc2)
+    os.remove(loc)
+
+
+xbmcvfs.copy(src, loc)
+
+zip = ZipFile(loc)
+zip.extractall(loc3)
+zip.close()
+
+
+for path, subdirs, files in os.walk(loc2):
+    for name in files:
+        print(os.path.join(path, name))
