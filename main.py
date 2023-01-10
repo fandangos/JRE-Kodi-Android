99f225d963247aa8615b5a15bb927c977cb6decc
Vitor DallAcqua <veggav@gmail.com>
2023-01-10 08:08:38 -0300

    Make installation persistent

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ from zipfile import ZipFile
 import shutil
 
 src = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'resources/lib/j2re-image.zip')
-loc = xbmcvfs.translatePath("special://xbmcbin/j2re-image.zip")
-loc2 = xbmcvfs.translatePath("special://xbmcbin/j2re-image/")
-loc3 = xbmcvfs.translatePath("special://xbmcbin/")
+loc = xbmcvfs.translatePath("special://xbmcbin/../../../cache/lib/j2re-image.zip")
+libbluray = xbmcvfs.translatePath("special://xbmcbin/../../../cache/lib/j2re-image/libbluray.jar")
+loc2 = xbmcvfs.translatePath("special://xbmcbin/../../../cache/lib/j2re-image/")
+loc3 = xbmcvfs.translatePath("special://xbmcbin/../../../cache/lib/")
 
-if xbmcvfs.exists(loc2):
-    shutil.rmtree(loc2)
-    os.remove(loc)
 
-if xbmcvfs.exists(src):
-    xbmcvfs.copy(src, loc)
-
-zip = ZipFile(loc)
-zip.extractall(loc3)
-zip.close()
+if xbmcvfs.exists(libbluray):
+    print("Java Runtime is already installed.")
+else:
+    if xbmcvfs.exists(src):
+        xbmcvfs.copy(src, loc)
+        zip = ZipFile(loc)
+        zip.extractall(loc3)
+        zip.close()
+    else:
+        print("Could not find j2re-image.zip")
 
 
 for path, subdirs, files in os.walk(loc2):
     for name in files:
         print(os.path.join(path, name))
-
